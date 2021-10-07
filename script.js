9f5490c71db1c1ac529ba96d95dfee8a4a38d17b
Spanishplz <spanishplz@gmail.com>
2021-10-06 19:45:50 -0500

    Program works as expected. Added the five matches and counter

--- script.js
+++ script.js
@@ -1,35 +1,78 @@
+fiveMatches();
 
+// plays the game 5 times and keeps the score
+function fiveMatches() {
 
+    let playerScore = 0;
+    let computerScore = 0;
 
+    for (i = 1; i <= 5; i++) {
+        console.log(`\n`);
+        console.log(`Round ${i}... FIGHT!`);
+        console.log(`\n`);
+        let result = game();
+        // console.log(result);
+        if (result === "tie") {
+            //score doesnt move
+        } else if (result === "win") {
+            playerScore += 1;
+        } else {
+            computerScore += 1;
+        }
+        console.log(`Computer score: ${computerScore}`);
+        console.log(`Player score: ${playerScore}`);
+    }
 
-let playerNoFormat = prompt("Type what you choose: rock, paper or scissors?");
-
-// Asks for some input, as long as it isn't null or an empty string
-while (playerNoFormat === null || playerNoFormat == "") {
-    alert('Please, enter one option! (Rock, paper or scissors)');
-    playerNoFormat = prompt("Type what you choose: rock, paper or scissors?");
+    if (playerScore === computerScore) {
+        console.log(`\n`);
+        console.log(`I thought you were better than this...`);
+        console.log(`FINAL SCORE: \n${computerScore} - ${playerScore}`);
+        console.log(`IT'S A TIIIIIIIIIIIIIEEEEEE! `);
+    } else if (playerScore > computerScore) {
+        console.log(`\n`);
+        console.log(`Not so much AI in here, eh? Hehehe`);
+        console.log(`FINAL SCORE: \n${computerScore} - ${playerScore}`);
+        console.log(`YOUUUUUU ARE THE CHAAAAAAAAMPIONNN!!!`);
+    } else {
+        console.log(`\n`);
+        console.log(`I don't wanna say I'm disappointed...but I a...m...`);
+        console.log(`FINAL SCORE: \n${computerScore} - ${playerScore}`);
+        console.log(`YOU LOOOOOOSE! LOOOOOSE....LOOOSE...LOOSE...`);
+    }
 }
 
-// formatting the user's input and receiving the input from the computerPlay()
-// function
-let playerSelection = playerNoFormat.toLowerCase();
-const computerSelection = computerPlay();
+function game() {
+    let playerNoFormat = prompt("Type what you choose: rock, paper or scissors?");
 
-// Makes you choose one valid option to continue, otherwise you stay here
-while (playerSelection != "rock" &&
-       playerSelection != "paper" &&
-       playerSelection != "scissors" &&
-       playerSelection != null) {
-    console.log(`${playerNoFormat} isn't a valid option, try again please!`);
-    playerNoFormat = prompt("Type what you choose: rock, paper or scissors?");
-    playerSelection = playerNoFormat.toLowerCase();
+    // Asks for some input, as long as it isn't null or an empty string
+    while (playerNoFormat === null || playerNoFormat == "") {
+        alert('Please, enter one option! (Rock, paper or scissors)');
+        playerNoFormat = prompt("Type what you choose: rock, paper or scissors?");
+    }
+
+    // formatting the user's input and receiving the input from the computerPlay()
+    // function
+    let playerSelection = playerNoFormat.toLowerCase();
+    const computerSelection = computerPlay();
+
+    // Makes you choose one valid option to continue, otherwise you stay here
+    while (playerSelection != "rock" &&
+           playerSelection != "paper" &&
+           playerSelection != "scissors" &&
+           playerSelection != null) {
+        console.log(`${playerNoFormat} isn't a valid option, try again please!`);
+        playerNoFormat = prompt("Type what you choose: rock, paper or scissors?");
+        playerSelection = playerNoFormat.toLowerCase();
+    }
+
+    toPrint(playerSelection, computerSelection);
+    let roundResult = playRound(playerSelection, computerSelection);
+    winnerRoundDecision(playerSelection, computerSelection, roundResult);
+    // console.log(roundResult);
+
+    return roundResult;
+    // console.log(roundResult);
 }
-
-toPrint(playerSelection, computerSelection);
-let roundResult = playRound(playerSelection, computerSelection);
-winnerRoundDecision(playerSelection, computerSelection, roundResult);
-// console.log(roundResult);
-
 
 
 // compares the selections and gives the score of one round
